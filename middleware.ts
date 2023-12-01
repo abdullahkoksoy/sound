8fcd42f288abb618df35a182c1a7d99de8224401
abdullahkoksoy <120575840+abdullahkoksoy@users.noreply.github.com>
2023-12-01 22:51:44 +0300

    com11

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,30 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextRequest, NextResponse } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession();
-  return res;
 
-};
+  // Retrieve Supabase URL and key from environment variables
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Supabase URL and/or key not provided');
+  }
+
+  // Set the Supabase URL and key as environment variables
+  process.env.SUPABASE_URL = supabaseUrl;
+  process.env.SUPABASE_KEY = supabaseKey;
+
+  // Create Supabase client using createMiddlewareClient
+  const supabase = createMiddlewareClient({ req, res });
+
+  // Use the supabase client as needed
+  await supabase.auth.getSession();
+
+  // Clean up environment variables if needed
+  delete process.env.SUPABASE_URL;
+  delete process.env.SUPABASE_KEY;
+
+  return res;
+}
