7136f8f58d1c5a2db74cbb5479caf65e62f03678
abdullahkoksoy <120575840+abdullahkoksoy@users.noreply.github.com>
2023-12-01 22:59:59 +0300

    com12

--- middleware.ts
+++ middleware.ts
@@ -12,19 +12,11 @@ export async function middleware(req: NextRequest) {
     throw new Error('Supabase URL and/or key not provided');
   }
 
-  // Set the Supabase URL and key as environment variables
-  process.env.SUPABASE_URL = supabaseUrl;
-  process.env.SUPABASE_KEY = supabaseKey;
-
   // Create Supabase client using createMiddlewareClient
   const supabase = createMiddlewareClient({ req, res });
 
   // Use the supabase client as needed
   await supabase.auth.getSession();
-
-  // Clean up environment variables if needed
-  delete process.env.SUPABASE_URL;
-  delete process.env.SUPABASE_KEY;
 
   return res;
 }
