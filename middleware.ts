a73d64e5d955a4c36b5bb6b0a9a987f816c9fe38
abdullahkoksoy <120575840+abdullahkoksoy@users.noreply.github.com>
2023-12-02 10:08:05 +0300

    com15

--- middleware.ts
+++ middleware.ts
@@ -8,15 +8,23 @@ export async function middleware(req: NextRequest) {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+  // Check if Supabase URL and key are provided
   if (!supabaseUrl || !supabaseKey) {
-    throw new Error('Supabase URL and/or key not provided');
+    const errorMessage = 'Supabase URL and/or key not provided';
+    console.error(errorMessage);
+    throw new Error(errorMessage);
   }
 
-  // Create Supabase client using createMiddlewareClient
-  const supabase = createMiddlewareClient({ req, res });
+  try {
+    // Create Supabase client using createMiddlewareClient
+    const supabase = createMiddlewareClient({ req, res });
 
-  // Use the supabase client as needed
-  await supabase.auth.getSession();
+    // Use the supabase client as needed
+    await supabase.auth.getSession();
 
-  return res;
+    return res;
+  } catch (error) {
+    console.error('Error in Supabase middleware:', error);
+    throw error; // Rethrow the error to propagate it further
+  }
 }
