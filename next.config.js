09db2ac8be2e4768351d2e6bc14fbe91a8366288
abdullahkoksoy <120575840+abdullahkoksoy@users.noreply.github.com>
2023-12-02 10:18:43 +0300

    com16

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
         domains: [
             "coognhshievguktujckq.supabase.co"
         ]
-    }
+    },
+    experimental: {
+        forceSwcTransforms: true,
+    },
 }
 
 module.exports = nextConfig
